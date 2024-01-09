ba9dd4921350bb1ef28631ebcf8157e6dbdc3564
cottonpup <younggyoung.lee1@gmail.com>
2024-01-09 09:34:08 +0900

    chore: 에러 메시지 코드변경

--- src/pages/api/posts/[postId].ts
+++ src/pages/api/posts/[postId].ts
@@ -15,47 +15,51 @@ interface PostType {
 const db = new Database('./data');
 const Post = db.table<PostType>('posts');
 
-const getPostById = async (postId: string) => {
-  const post = await Post.findById(postId);
-  return post;
-};
-
-const sendErrorResponse = (res: NextApiResponse<Data>, message: string, statusCode: number) => {
-  res.status(statusCode).json({ message });
-};
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const { postId } = req.query;
   const { title, description } = req.body;
 
   switch (req.method) {
     case 'GET':
-      const post = await getPostById(postId as string);
-      post ? res.status(200).json({ post }) : sendErrorResponse(res, '포스트를 찾을 수 없습니다.', 404);
+      try {
+        const post = await Post.findById(postId as string);
+        if (!post) {
+          return res.status(404).json({ message: '포스트를 찾을 수 없습니다.' });
+        }
+        res.status(200).json({ post });
+      } catch (error) {
+        res.status(500).json({ message: '서버 에러가 발생했습니다.' });
+      }
       break;
 
     case 'DELETE':
-      const postToDelete = await getPostById(postId as string);
-      if (!postToDelete) {
-        sendErrorResponse(res, '삭제할 포스트를 찾을 수 없습니다.', 404);
-        return;
+      try {
+        const postToDelete = await Post.findById(postId as string);
+        if (!postToDelete) {
+          return res.status(404).json({ message: '삭제할 포스트를 찾을 수 없습니다.' });
+        }
+        await Post.delete(postId as string);
+        res.status(200).json({ message: '포스트가 삭제되었습니다.' });
+      } catch (error) {
+        res.status(500).json({ message: '서버 에러가 발생했습니다.' });
       }
-      await Post.delete(postId as string);
-      res.status(200).json({ message: '포스트가 삭제되었습니다.' });
       break;
 
     case 'PUT':
-      const postToUpdate = await getPostById(postId as string);
-      if (!postToUpdate) {
-        sendErrorResponse(res, '업데이트할 포스트를 찾을 수 없습니다.', 404);
-        return;
+      try {
+        const postToUpdate = await Post.findById(postId as string);
+        if (!postToUpdate) {
+          return res.status(404).json({ message: '업데이트할 포스트를 찾을 수 없습니다.' });
+        }
+        const updatedPost = await Post.update({ id: postId as string, title, description });
+        res.status(200).json({ post: updatedPost });
+      } catch (error) {
+        res.status(500).json({ message: '서버 에러가 발생했습니다.' });
       }
-      const updatedPost = await Post.update({ id: postId as string, title, description });
-      res.status(200).json({ post: updatedPost });
       break;
 
     default:
       res.setHeader('Allow', ['GET', 'DELETE', 'PUT']);
-      res.status(405).end(`${req.method}는 허용되지 않습니다.`);
+      res.status(405).json({ message: `${req.method}는 허용되지 않습니다.` });
   }
 }
