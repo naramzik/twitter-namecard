b4591c61f670e20ba12b0ebb39f94232b96069da
biyamn <b1y9mn@gmail.com>
2024-01-10 17:51:34 +0900

    feat: fieldset으로 sns 입력란 묶기

--- src/pages/[id]/edit/index.tsx
+++ src/pages/[id]/edit/index.tsx
@@ -5,7 +5,9 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 type FormValues = {
   nickname: string;
   twitterId: string;
-  snsId: string;
+  instagramId: string;
+  githubId: string;
+  blogId: string;
   hashtag: string;
 };
 
@@ -13,7 +15,9 @@ const Page = () => {
   const [formData, setFormData] = useState<FormValues>({
     nickname: '',
     twitterId: '',
-    snsId: '',
+    instagramId: '',
+    githubId: '',
+    blogId: '',
     hashtag: '',
   });
 
@@ -26,7 +30,9 @@ const Page = () => {
     defaultValues: {
       nickname: '',
       twitterId: '',
-      snsId: '',
+      instagramId: '',
+      githubId: '',
+      blog: '',
       hashtag: '',
     },
   });
@@ -39,10 +45,12 @@ const Page = () => {
   return (
     <>
       <div className="border-2 border-black aspect-nameCard">
-        <div>{formData.nickname}</div>
-        <div>{formData.twitterId}</div>
-        <div>{formData.hashtag}</div>
-        <div>{formData.snsId}</div>
+        <div>닉네임: {formData.nickname}</div>
+        <div>트위터 아이디: {formData.twitterId}</div>
+        <div>해시태그: {formData.hashtag}</div>
+        <div>인스타 아이디: {formData.instagramId}</div>
+        <div>깃허브 아이디: {formData.githubId}</div>
+        <div>블로그 주소: {formData.blog}</div>
       </div>
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
         <label>닉네임</label>
@@ -53,13 +61,19 @@ const Page = () => {
         <input {...register('twitterId', { required: true, maxLength: 10 })} />
         {errors.twitterId && requiredSentence}
 
-        <label>SNS 아이디</label>
-        <input {...register('snsId', { required: true, maxLength: 10 })} />
-        {errors.snsId && requiredSentence}
-
         <label>해시태그</label>
         <input {...register('hashtag', { required: true, maxLength: 10 })} />
         {errors.hashtag && requiredSentence}
+        <fieldset className="flex flex-col border border-solid border-gray-300 p-2">
+          <legend>SNS 아이디</legend>
+
+          <label>인스타</label>
+          <input {...register('instagramId', { required: false, maxLength: 10 })} />
+          <label>깃허브</label>
+          <input {...register('githubId', { required: false, maxLength: 10 })} />
+          <label>블로그</label>
+          <input {...register('blog', { required: false, maxLength: 10 })} />
+        </fieldset>
 
         <button type="submit" className="btm-nav btm-nav-md max-w-[512px] mx-auto z-20 bg-accent text-white font-bold">
           저장하기
