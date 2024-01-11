dddce9fb04cbb84968815f9baccb8ae21c09dbc7
cottonpup <younggyoung.lee1@gmail.com>
2024-01-11 10:07:59 +0900

    feat: errorHandler 추가

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function errorHandler(req: NextApiRequest, res: NextApiResponse, fn: () => Promise<void>) {
+  try {
+    await fn();
+  } catch (error) {
+    res.status(500).json({ message: '서버 에러가 발생했습니다.' });
+  }
+}
