c4656a5562efbf8e260f34c2e7b738e73b8b338e
biyamn <b1y9mn@gmail.com>
2024-01-14 15:11:24 +0900

    refactor: watch로 state 대체

--- src/pages/[id]/edit/index.tsx
+++ src/pages/[id]/edit/index.tsx
@@ -1,16 +1,15 @@
 import { ReactNode, useRef, useState } from 'react';
-// import { SubmitHandler, useForm } from 'react-hook-form';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import BasicLayout from '@/components/layout/BasicLayout';
 
-// interface FormValues {
-//   nickname: string;
-//   twitterId: string;
-//   instagramId: string;
-//   githubId: string;
-//   blog: string;
-//   hashtag: string;
-// }
+interface Data {
+  nickname: string;
+  twitterId: string;
+  instagramId: string;
+  githubId: string;
+  blog: string;
+  hashtag: string;
+}
 
 interface FreeItemProp {
   index: number;
@@ -27,7 +26,8 @@ const Page = () => {
 
   const {
     register,
-    // handleSubmit,
+    handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -69,12 +69,12 @@ const Page = () => {
     );
   };
 
-  // <FreeItem /> 때문에 useState를 가장 위로 빼줄 수 없었음
+  // <FreeItem /> 아래에 있어야 해서 useState를 가장 위로 빼줄 수 없었음
   const [freeItems, setFreeItems] = useState<JSX.Element[]>([<FreeItem index={0} />]);
 
-  // const onSubmit: SubmitHandler<FormValues> = (data) => {
-  //   // setFormData(data);
-  // };
+  const onSubmit = (data: Data) => {
+    console.log(data);
+  };
 
   const addFreeItem = () => {
     countRef.current += 1;
@@ -84,45 +84,52 @@ const Page = () => {
 
   const requiredSentence = <p>필수 문항입니다.</p>;
 
+  const nickname = watch('nickname');
+  const twitterId = watch('twitterId');
+  const instagramId = watch('instagramId');
+  const githubId = watch('githubId');
+  const blog = watch('blog');
+  const hashtag = watch('hashtag');
+
   return (
     <div className="pb-12">
       <div className="border-2 border-black aspect-nameCard">
-        {/* <div>닉네임: {formData.nickname}</div>
-        <div>트위터 아이디: {formData.twitterId}</div>
-        <div>해시태그: {formData.hashtag}</div>
-        <div>인스타 아이디: {formData.instagramId}</div>
-        <div>깃허브 아이디: {formData.githubId}</div>
-        <div>블로그 주소: {formData.blog}</div> */}
+        <div>닉네임: {nickname}</div>
+        <div>트위터 아이디: {twitterId}</div>
+        <div>해시태그: {hashtag}</div>
+        <div>인스타 아이디: {instagramId}</div>
+        <div>깃허브 아이디: {githubId}</div>
+        <div>블로그 주소: {blog}</div>
       </div>
       {/* <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 py-5"> */}
       <label>
         닉네임
-        <input {...register('nickname', { required: true, maxLength: 10 })} />
+        <input {...register('nickname', { required: true, maxLength: 10 })} name="nickname" />
         {errors.twitterId && requiredSentence}
       </label>
       <label>
         트위터 아이디
-        <input {...register('twitterId', { required: true, maxLength: 10 })} />
+        <input {...register('twitterId', { required: true, maxLength: 10 })} name="twitterId" />
         {errors.twitterId && requiredSentence}
       </label>
       <label>
         해시태그
-        <input {...register('hashtag', { required: true, maxLength: 10 })} />
+        <input {...register('hashtag', { required: true, maxLength: 10 })} name="hashtag" />
         {errors.hashtag && requiredSentence}
       </label>
       <fieldset className="flex flex-col gap-2">
         <legend>SNS 아이디</legend>
         <label>
           인스타
-          <input {...register('instagramId', { required: false, maxLength: 10 })} />
+          <input {...register('instagramId', { required: false, maxLength: 10 })} name="instagramId" />
         </label>
         <label>
           깃허브
-          <input {...register('githubId', { required: false, maxLength: 10 })} />
+          <input {...register('githubId', { required: false, maxLength: 10 })} name="githubId" />
         </label>
         <label>
           블로그
-          <input {...register('blog', { required: false, maxLength: 10 })} />
+          <input {...register('blog', { required: false, maxLength: 10 })} name="blog" />
         </label>
       </fieldset>
       {freeItems}
