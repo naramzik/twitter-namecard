b6455213851caa0737bf5f4a6862fff535d3b3c2
cottonpup <younggyoung.lee1@gmail.com>
2024-01-16 11:48:19 +0900

    chore: 리퀘스트 명시

--- src/pages/api/twitterInfo/index.ts
+++ src/pages/api/twitterInfo/index.ts
@@ -4,29 +4,39 @@ import errorHandler from '@/utils/errorHandler';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { nickname } = req.body;
+  switch (req.method) {
+    case 'GET': {
+      await errorHandler(req, res, async () => {
+        const { nickname } = req.body;
+        if (!nickname) {
+          return res.status(400).json({
+            message: '유저이름을 적어주세요.',
+          });
+        }
 
-  if (!nickname) {
-    res.status(400).json({
-      message: '유저이름을 적어주세요.',
-    });
+        const url = `https://nitter.mint.lgbt/${nickname}`;
+        const response = await axios.get(url, {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/120.0.0.0 Mobile/15E148 Safari/604.1',
+          },
+        });
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        const bio = $(`.profile-bio`).text();
+        const image = 'https://nitter.mint.lgbt' + $('.profile-card-avatar').attr('href');
+
+        res.status(200).json({
+          bio,
+          image,
+        });
+      });
+      break;
+    }
+
+    default:
+      res.setHeader('Allow', ['GET']);
+      res.status(405).json(`${req.method}는 허용되지 않습니다.`);
   }
-
-  const url = `https://nitter.1d4.us/${nickname}`;
-  return errorHandler(req, res, async () => {
-    const response = await axios.get(url, {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/120.0.0.0 Mobile/15E148 Safari/604.1',
-      },
-    });
-    const html = response.data;
-    const $ = cheerio.load(html);
-
-    const bio = $(`.profile-bio`);
-
-    res.status(200).json({
-      bio: bio.text(),
-    });
-  });
 }
