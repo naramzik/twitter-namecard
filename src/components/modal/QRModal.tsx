54f168a646e50ee5a2a6f192023be4cc5cd2bae3
biyamn <b1y9mn@gmail.com>
2024-01-23 11:03:15 +0900

    fix: qr모달 오버레이 클릭시 안닫히는 오류 해결
    - ref를 사용하는 방식으로 변경

--- src/components/modal/QRModal.tsx
+++ src/components/modal/QRModal.tsx
@@ -1,19 +1,23 @@
 import NiceModal, { useModal } from '@ebay/nice-modal-react';
 import { QRCodeCanvas } from 'qrcode.react';
+import { useRef } from 'react';
 
 const QRModal = () => {
   const modal = useModal();
+  const qrModalRef = useRef(null);
 
-  const handleCloseQRModal = () => {
+  const handleCloseQRModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.target !== qrModalRef.current) return;
     modal.remove();
   };
+
   return (
     <>
-      <div
-        className="max-w-lg w-full min-h-screen mx-auto fixed bottom-0 left-0 right-0 opacity-60 bg-slate-900 z-30"
+      <section
+        className="w-full h-[calc(100vh-10rem)] z-50 flex fixed top-0 left-0 items-center justify-center flex-col"
         onClick={handleCloseQRModal}
-      />
-      <section className="w-full h-[calc(100vh-10rem)] z-50 flex fixed top-0 left-0 items-center justify-center flex-col">
+        ref={qrModalRef}
+      >
         <h1 className="text-primary text-4xl mb-4 font-bold text-center">QR CODE</h1>
         <QRCodeCanvas
           className="border-primary border-4 rounded-xl"
