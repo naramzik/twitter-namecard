46d88b63863e9b8a037b6c37f7c77759fe456163
biyam <101965666+biyamn@users.noreply.github.com>
2024-01-31 09:28:15 +0900

    Feat: qr코드 페이지 추가 (#9)
    * feat: qr코드 페이지 추가
    
    * chore: qrcode.react 설치
    
    * feat: qrcode 컴포넌트 기본 설정 추가
    
    * feat: qr코드 모달 추가 및 적용
    
    * feat: qr 모달 위치 중앙으로 조정
    
    * fix: qr모달 오버레이 클릭시 안닫히는 오류 해결
    
    - ref를 사용하는 방식으로 변경
    
    * chore: gitignore에 .env 파일 추가
    
    * chore: axios, react-query 설치
    
    * feat: 숏링크 생성 api 연결
    
    * fix: 오타 수정(sort => short)
    
    * feat: getServerSideProps 삭제 및 오타 수정
    
    * feat: 숏링크를 만드는 커스텀훅 추가
    
    * feat: qr코드로 공유하기 클릭시 숏링크 생성
    
    * feat: queryClientProvider 추가
    
    * chore: 할 일 주석 추가
    
    * refactor: post 요청 보내는 함수 분리

---
+++ src/hooks/queries/useCreateShortLink.ts
@@ -0,0 +1,17 @@
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface CardId {
+  cardId: string;
+}
+
+const getShortLink = async (cardId: CardId) => {
+  const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/short-link`, cardId);
+  return data;
+};
+
+export const useCreateShortLink = () => {
+  return useMutation({
+    mutationFn: getShortLink,
+  });
+};
---
+++ src/types/shortLink.ts
@@ -0,0 +1,3 @@
+export interface ShortLink {
+  shortLink: string;
+}
