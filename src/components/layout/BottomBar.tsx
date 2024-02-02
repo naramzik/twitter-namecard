53dce24a780523ffdb5a1b7839644c67d7a767b2
biyamn <b1y9mn@gmail.com>
2024-02-02 10:02:14 +0900

    feat: 내 명함도 만들어보기 클릭시 '/[id]/edit'으로 이동

--- src/components/layout/BottomBar.tsx
+++ src/components/layout/BottomBar.tsx
@@ -1,6 +1,20 @@
+import { useRouter } from 'next/router';
+
 const BottomBar = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const cardCreateHandler = () => {
+    router.push(`/${id}/edit`);
+  };
+
   return (
-    <div className="btm-nav btm-nav-md max-w-lg mx-auto z-20 bg-accent text-white font-bold">내 명함도 만들어보기</div>
+    <button
+      onClick={cardCreateHandler}
+      className="btm-nav btm-nav-md max-w-lg mx-auto z-20 bg-accent text-white font-bold"
+    >
+      내 명함도 만들어보기
+    </button>
   );
 };
 
