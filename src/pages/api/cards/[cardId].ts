0692d5729e3319fbc8364f1a866fb96480f75751
cottonpup <younggyoung.lee1@gmail.com>
2024-02-08 00:27:44 +0900

    feat: 동적 라우팅 폴더명 변경

--- src/pages/api/cards/[cardId].ts
+++ src/pages/api/cards/[cardId].ts
@@ -1,62 +1,40 @@
-import { Database } from 'fakebase';
-import { Entity } from '@/types/Entity';
 import errorHandler from '@/utils/errorHandler';
+import supabase from '@/utils/supabase';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-interface CardType extends Entity {
-  nickname: string;
-  twitter: string;
-  hashtags?: string[];
-  socialMedia?: SocialMedia;
-  customFields?: CustomFields[];
-}
-
-interface SocialMedia {
-  instagram: string;
-  github: string;
-  blog: string;
-}
-
-interface CustomFields {
-  id: string;
-  key: string;
-  contents: string;
-}
-const db = new Database('./data');
-const Card = db.table<CardType>('cards');
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const cardId = req.query.id as string;
-  const twitterId = req.query.id as string;
+  const cardId = req.query.cardId as string;
 
   switch (req.method) {
     case 'GET':
       await errorHandler(req, res, async () => {
-        const foundCard = await Card.findOne((card) => card.twitter === twitterId);
+        const { data: foundCard, error } = await supabase.from('cards').select('*').eq('id', cardId).single();
+        if (error) throw error;
         res.status(200).json({ foundCard });
       });
       break;
 
     case 'DELETE':
       await errorHandler(req, res, async () => {
-        const cardToDelete = await Card.findById(cardId);
+        const { data: cardToDelete, error } = await supabase.from('cards').delete().eq('id', cardId).select();
+        if (error) throw error;
         if (!cardToDelete) {
           return res.status(404).json({ message: '삭제할 명함을 찾을 수 없습니다.' });
         }
-        await Card.delete(cardId);
         res.status(200).json({ message: '명함이 삭제되었습니다.' });
       });
       break;
 
     case 'PUT':
       await errorHandler(req, res, async () => {
-        const cardToUpdate = await Card.findById(cardId);
-        if (!cardToUpdate) {
+        const { data: updatedCard, error } = await supabase.from('cards').update(req.body).eq('id', cardId).select();
+
+        if (error) throw error;
+        if (!updatedCard) {
           return res.status(404).json({ message: '업데이트할 명함을 찾을 수 없습니다.' });
         }
-        const updatedCardData = { ...cardToUpdate, ...req.body };
-        const updatedCard = await Card.update({ id: cardId, ...updatedCardData });
-        res.status(200).json({ card: updatedCard });
+
+        res.status(200).json(updatedCard);
       });
       break;
 
