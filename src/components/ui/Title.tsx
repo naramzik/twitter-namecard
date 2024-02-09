e5551987e46a7f78aafb7d999a3a25feed353d06
biyamn <b1y9mn@gmail.com>
2024-02-09 09:17:25 +0900

    refactor: html 태그 변경
    - div => button

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -13,7 +13,7 @@ const Title = ({ title }: Title) => {
   };
 
   return (
-    <div className="fixed top-0 z-10 grid items-center w-full max-w-[512px] mx-auto left-0 right-0 grid-cols-3 h-16 bg-primary text-white">
+    <button className="fixed top-0 z-10 grid items-center w-full max-w-[512px] mx-auto left-0 right-0 grid-cols-3 h-16 bg-primary text-white">
       <Image
         width={25}
         height={25}
@@ -23,7 +23,7 @@ const Title = ({ title }: Title) => {
         alt="뒤로가기"
       />
       <div className="text-lg text-center">{title}</div>
-    </div>
+    </button>
   );
 };
 export default Title;
