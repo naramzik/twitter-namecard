17bf66d868a0e6b673c6d5760bd508139997d7fc
biyamn <b1y9mn@gmail.com>
2024-02-22 19:02:11 +0900

    feat: 높이 조정

--- src/pages/default/index.tsx
+++ src/pages/default/index.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 const Page = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-full gap-20">
-      <Image src="/empty.png" alt="우주선" width={200} height={200} />
-      <h1 className="text-xl font-bold">홈에서 명함을 선택해보세요! 👩‍🚀</h1>
+    <div className="flex flex-col justify-center items-center gap-20 h-[90vh]">
+      <Image src="/empty.png" alt="우주선" width={150} height={150} />
+      <h1 className="text-xl font-bold">홈에서 명함을 선택해 보세요! 👩‍🚀</h1>
     </div>
   );
 };
