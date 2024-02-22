6c432f3a8f4dc0c508030a2eaab8f08232ce8c0f
biyamn <b1y9mn@gmail.com>
2024-02-22 19:39:30 +0900

    feat: 배경색, 아이콘 크기 변경

--- src/components/icons/CardIcon.tsx
+++ src/components/icons/CardIcon.tsx
@@ -2,7 +2,7 @@ import type { NavIconProps } from '@/components/ui/BottomNavigation';
 
 export default function CreateIcon({ color = '#BDBDBD', ...props }: NavIconProps) {
   return (
-    <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg width="45" height="45" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         d="M2 12C2 8.22876 2 6.34315 3.17157 5.17157C4.34315 4 6.22876 4 10 4H14C17.7712 4 19.6569 4 20.8284 5.17157C22 6.34315 22 8.22876 22 12C22 15.7712 22 17.6569 20.8284 18.8284C19.6569 20 17.7712 20 14 20H10C6.22876 20 4.34315 20 3.17157 18.8284C2 17.6569 2 15.7712 2 12Z"
         stroke={color}
