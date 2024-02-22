4bcb7cb695e17c8ffd54695623348bcb037ae07f
biyamn <b1y9mn@gmail.com>
2024-02-22 19:56:39 +0900

    feat: 명함 목록 불러오기 toast 메시지 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,18 @@ import CardItem from '@/components/card/CardItem';
 import LayoutWithHeader from '@/components/layout/LayoutWithHeader';
 import { useGetCards } from '@/hooks/queries/useGetCards';
 import type { CardType } from '@/types/cards';
+import { showToastLoadingMessage, showToastSuccessMessage } from '@/utils/showToastMessage';
 
 export default function Home() {
-  const { cards } = useGetCards();
+  const { cards, isLoading, isSuccess } = useGetCards();
+
+  if (isLoading) showToastLoadingMessage('트위터 명함 목록을 불러오는 중이에요.');
+  if (isSuccess) showToastSuccessMessage('트위터 명함 목록을 불러왔어요!');
 
   return (
-    <>
+    <div>
       <main className="flex flex-col gap-5 mx-1">{cards?.map((card: CardType) => <CardItem card={card} />)}</main>
-    </>
+    </div>
   );
 }
 
