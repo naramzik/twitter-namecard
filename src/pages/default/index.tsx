f317d92cdfb618f6225769cff145555a1b98898b
biyamn <b1y9mn@gmail.com>
2024-02-22 18:54:28 +0900

    feat: 명함 상세 페이지 기본 버전 추가

--- src/pages/default/index.tsx
+++ src/pages/default/index.tsx
@@ -1,7 +1,13 @@
 import LayoutWithHeader from '@/components/layout/LayoutWithHeader';
+import Image from 'next/image';
 
 const Page = () => {
-  return <>홈에서 명함을 선택해보세요!</>;
+  return (
+    <div className="flex flex-col justify-center items-center h-full gap-20">
+      <Image src="/empty.png" alt="우주선" width={200} height={200} />
+      <h1 className="text-xl font-bold">홈에서 명함을 선택해보세요! 👩‍🚀</h1>
+    </div>
+  );
 };
 
 Page.getLayout = function getLayout(page) {
