7ee54bc7864ff86e1358ddf381b3aeac701b1a4b
cottonpup <younggyoung.lee1@gmail.com>
2024-02-22 18:11:35 +0900

    feat: 자유형식 지우는 버튼 변경

--- src/components/card/CardForm.tsx
+++ src/components/card/CardForm.tsx
@@ -473,7 +473,20 @@ const CardForm = ({ cardId }: { cardId: string | null }) => {
                     )
                   }
                 >
-                  -
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 hover:text-red-700 active:text-red-700"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
                 </button>
               </div>
             </div>
