0926e70a69a69a28fe16e6a8f5628997299d9300
cottonpup <younggyoung.lee1@gmail.com>
2024-02-23 11:11:09 +0900

    feat: 트위터 크롤링 API 를 PUT 메서드랑 합치기

--- src/pages/api/cards/[cardId].ts
+++ src/pages/api/cards/[cardId].ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import errorHandler from '@/utils/errorHandler';
 import supabase from '@/utils/supabase';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -27,14 +28,51 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     case 'PUT':
       await errorHandler(req, res, async () => {
-        const { data: updatedCard, error } = await supabase.from('cards').update(req.body).eq('id', cardId).select();
+        // const { data: updatedCard, error } = await supabase.from('cards').update(req.body).eq('id', cardId).select();
 
-        if (error) throw error;
-        if (!updatedCard) {
-          return res.status(404).json({ message: '업데이트할 명함을 찾을 수 없습니다.' });
+        const { customImage, twitterProfile, ...rest } = req.body;
+        // 트위터 프로필 이미지일 떄,
+        if (!customImage) {
+          const imageDownloadResponse = await axios.get(twitterProfile, {
+            responseType: 'arraybuffer',
+          });
+          const imageBuffer = Buffer.from(imageDownloadResponse.data, 'binary');
+
+          const { error: uploadError } = await supabase.storage.from('image_url').upload(cardId, imageBuffer, {
+            upsert: true,
+          });
+          if (uploadError) {
+            console.error('이미지 파일을 올릴 수 없습니다.:', uploadError);
+            return;
+          }
+        } else {
+          // 커스텀 이미지를 추가할 때
+          const { error: uploadError } = await supabase.storage.from('image_url').upload(cardId, customImage, {
+            upsert: true,
+          });
+          if (uploadError) {
+            console.error('이미지 파일을 올릴 수 없습니다.:', uploadError);
+            return;
+          }
+        }
+        // Supabase Storage에서 공개적으로 접근가능한 URL 생성하기
+        const { data: imageUrl } = supabase.storage.from('image_url').getPublicUrl(cardId);
+        //  Supbase Table에 저장하기
+        const { data: updatedCard, error: updatedCardError } = await supabase
+          .from('cards')
+          .update({ image_url: imageUrl.publicUrl, ...rest })
+          .eq('id', cardId)
+          .select();
+
+        if (updatedCardError) throw updatedCardError;
+        if (!updatedCard || updatedCard.length === 0) {
+          // 업데이트할 카드가 없는 경우
+          return res.status(404).json({ message: '카드를 찾을 수 없습니다.' });
         }
 
-        res.status(200).json(updatedCard);
+        res.status(200).json({
+          updatedCard,
+        });
       });
       break;
 
@@ -44,3 +82,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       break;
   }
 }
+
+// case 'POST':
+// await errorHandler(req, res, async () => {
+//   const { customImage, cardId } = req.body;
+//   if (!customImage) {
+//     const imageDownloadResponse = await axios.get(image, {
+//       responseType: 'arraybuffer',
+//     });
+
+//     const imageBuffer = Buffer.from(imageDownloadResponse.data, 'binary');
+
+//     const { error: uploadError } = await supabase.storage.from('image_url').upload(cardId, imageBuffer, {
+//       upsert: true,
+//     });
+//     if (uploadError) {
+//       console.error('이미지 파일을 올릴 수 없습니다.:', uploadError);
+//       return;
+//     }
+//   } else {
+//     const { error: uploadError } = await supabase.storage.from('image_url').upload(cardId, customImage, {
+//       upsert: true,
+//     });
+//     if (uploadError) {
+//       console.error('이미지 파일을 올릴 수 없습니다.:', uploadError);
+//       return;
+//     }
+//   }
