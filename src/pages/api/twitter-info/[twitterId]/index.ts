69e7bf240884090e26acb5aac81998a78201332a
Younggyoung Lee <67526014+cottonpup@users.noreply.github.com>
2024-02-25 00:05:17 +0900

    feat: 카드 업데이트 API PUT 메서드에 트위터 크롤링 저장 API 합치기 (#64)

--- src/pages/api/twitter-info/[twitterId]/index.ts
+++ src/pages/api/twitter-info/[twitterId]/index.ts
@@ -1,7 +1,5 @@
-import axios from 'axios';
 import * as cheerio from 'cheerio';
 import errorHandler from '@/utils/errorHandler';
-import supabase from '@/utils/supabase';
 import switchCrawler, { currentInstanceIndex, instanceUrls } from '@/utils/switchCrawler';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
@@ -34,61 +32,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         });
         break;
 
-      case 'POST':
-        await errorHandler(req, res, async () => {
-          const { customImage, cardId } = req.body;
-          if (!customImage) {
-            const imageDownloadResponse = await axios.get(image, {
-              responseType: 'arraybuffer',
-            });
-
-            const imageBuffer = Buffer.from(imageDownloadResponse.data, 'binary');
-
-            const { error: uploadError } = await supabase.storage.from('image_url').upload(cardId, imageBuffer, {
-              upsert: true,
-            });
-            if (uploadError) {
-              console.error('이미지 파일을 올릴 수 없습니다.:', uploadError);
-              return;
-            }
-          } else {
-            const { error: uploadError } = await supabase.storage.from('image_url').upload(cardId, customImage, {
-              upsert: true,
-            });
-            if (uploadError) {
-              console.error('이미지 파일을 올릴 수 없습니다.:', uploadError);
-              return;
-            }
-          }
-
-          // Supabase Storage에서 공개적으로 접근가능한 URL 생성하기
-          const { data: imageUrl } = supabase.storage.from('image_url').getPublicUrl(cardId);
-          //  Supbase Table에 저장하기
-          const { data: updatedCard, error: updatedCardError } = await supabase
-            .from('cards')
-            .update({ image_url: imageUrl.publicUrl, bio, nickname })
-            .eq('id', cardId)
-            .select();
-
-          if (updatedCardError) throw updatedCardError;
-          if (!updatedCard || updatedCard.length === 0) {
-            // 업데이트할 카드가 없는 경우
-            return res.status(404).json({ message: '카드를 찾을 수 없습니다.' });
-          }
-
-          res.status(200).json({
-            updatedCard,
-          });
-        });
-        break;
-
       default:
-        res.setHeader('Allow', ['GET', 'POST']);
+        res.setHeader('Allow', ['GET']);
         res.status(405).json(`${req.method}는 허용되지 않습니다.`);
     }
   } catch (error) {
     if (error) {
-      throw res.status(404).json({ message: '카드를 찾을 수 없습니다.' });
+      throw res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
     }
   }
 }
