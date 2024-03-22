d86f21404c3608f4468337b8432825776f12bcfe
biyamn <b1y9mn@gmail.com>
2024-03-22 20:55:36 +0900

    chore: console.log 삭제

--- src/components/modal/BottomSheet.tsx
+++ src/components/modal/BottomSheet.tsx
@@ -14,7 +14,6 @@ const BottomSheet = ({ nickname, cardId }: { nickname: string; cardId: string })
       { cardId },
       {
         onSuccess: (data) => {
-          console.log('data: ', data);
           NiceModal.show(QRModal, { shortLink: data[0].shortLink });
         },
       },
