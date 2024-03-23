f67bb46c7eeb92bea47a522fb9b2543080d9043c
nabi.lee <hello@nabi.kim>
2024-03-23 18:47:04 +0900

    fix: pass instaceIndex params to switchCrawler

--- src/utils/switchCrawler.ts
+++ src/utils/switchCrawler.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 export const instanceUrls = (process.env.NITTER_HOST ?? '').split(';');
 
-export default async function switchCrawler(param: string | null) {
-  let currentInstanceIndex = 0;
-
+export default async function switchCrawler(param: string | null, currentInstanceIndex = 0) {
   const USER_AGENT =
     'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/120.0.0.0 Mobile/15E148 Safari/604.1';
+
   try {
     const response = await axios.get(instanceUrls[currentInstanceIndex] + param, {
       headers: {
@@ -14,8 +13,6 @@ export default async function switchCrawler(param: string | null) {
     });
     return { data: response.data, instanceUrl: instanceUrls[currentInstanceIndex] };
   } catch (error) {
-    currentInstanceIndex = (currentInstanceIndex + 1) % instanceUrls?.length;
-    console.error(error);
-    return switchCrawler(param);
+    return switchCrawler(param, (currentInstanceIndex + 1) % instanceUrls?.length);
   }
 }
