1226495213963b59988157ef9350806f0a6be332
nabi.lee <hello@nabi.kim>
2024-03-23 18:09:11 +0900

    refactor /cards/[cardId]/logout

--- src/pages/api/cards/[cardId]/logout.ts
+++ src/pages/api/cards/[cardId]/logout.ts
@@ -3,21 +3,22 @@ import supabase from '@/utils/supabase';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  switch (req.method) {
-    case 'POST':
-      await errorHandler(req, res, async () => {
+  await errorHandler(req, res, async () => {
+    switch (req.method) {
+      case 'POST': {
         const { error } = await supabase.auth.signOut();
 
         if (error) {
-          return res.status(401).json('로그아웃을 실패했습니다.');
+          return res.status(401).json({ message: '로그아웃을 실패했습니다.' });
         }
 
-        return res.status(201).json('로그아웃에 성공했습니다.');
-      });
-      break;
+        return res.status(201).json({ message: '로그아웃에 성공했습니다.' });
+      }
 
-    default:
-      res.setHeader('Allow', ['POST']);
-      res.status(405).json({ message: `${req.method}는 허용되지 않습니다.` });
-  }
+      default: {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).json({ message: `${req.method}는 허용되지 않습니다.` });
+      }
+    }
+  });
 }
