3243879e020128e5eeed0c5cd84760b29ea4147d
cottonpup <younggyoung.lee1@gmail.com>
2024-03-26 20:25:31 +0900

    fix: 에러핸들러 로그출력 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    // 1. api/short-link 로 이동
+    // 2. API 내부에서 redirect 시키기
+    return [
+      {
+        source: '/card/:cardId*',
+        destination: '/api/short-link/:cardId*?shortLink=cardId',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
