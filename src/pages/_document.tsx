252cdec2be7970ca593e29bbdad12a12e817ea95
cottonpup <younggyoung.lee1@gmail.com>
2024-03-27 11:12:50 +0900

    feat: 구글 애널리틱스 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,15 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              `,
+          }}
+        />
       </body>
     </Html>
   );
