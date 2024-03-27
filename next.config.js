551356ce58686294a8d58d37f6acdc6c25dc6029
cottonpup <younggyoung.lee1@gmail.com>
2024-03-27 10:00:25 +0900

    fix: 리라이트 코드 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/s/:cardId*',
+        destination: '/api/short-link/:cardId*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
