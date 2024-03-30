e60193dc2f302074d5bd365fbff181517153cc31
biyamn <b1y9mn@gmail.com>
2024-03-30 20:34:38 +0900

    feat: 커피한잔 레이아웃 변경

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -9,7 +9,7 @@ const HomeLayout = ({ children }: LayoutWithHeaderProps) => {
   return (
     <>
       {process.env.NEXT_PUBLIC_BUYMEACOFFEE_URL && (
-        <aside className="bg-yellow-500 font-bold p-2 text-black text-center">
+        <aside className="bg-yellow-500 font-bold p-3 text-black text-center text-sm">
           서비스가 마음에 드신다면...{' '}
           <a href={process.env.NEXT_PUBLIC_BUYMEACOFFEE_URL} className="link underline-offset-4">
             커피 한잔
