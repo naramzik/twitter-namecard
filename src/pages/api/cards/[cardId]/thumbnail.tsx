6e29e97cf518b1a4a2ffec976a3270cd8130e177
cottonpup <younggyoung.lee1@gmail.com>
2024-03-31 11:33:04 +0900

    fix: 명함 OG 이미지 스타일 변경

--- src/pages/api/cards/[cardId]/thumbnail.tsx
+++ src/pages/api/cards/[cardId]/thumbnail.tsx
@@ -47,13 +47,13 @@ export default async function handler(request: NextRequest) {
             }}
           >
             <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <span style={{ fontSize: '2.5em', lineHeight: 1, fontWeight: 'bold' }}>{foundCard.nickname}</span>
-              <span style={{ fontSize: '1.5em', lineHeight: 1 }}>@{foundCard.twitter ?? 'anonymous'}</span>
-              {foundCard.bio && <span style={{ fontSize: '1em', marginTop: '0.5em' }}>"{foundCard.bio}"</span>}
+              <span style={{ fontSize: '2em', lineHeight: 1, fontWeight: 'bold' }}>{foundCard.nickname}</span>
+              <span style={{ fontSize: '1em', lineHeight: 1 }}>@{foundCard.twitter ?? 'anonymous'}</span>
+              {foundCard.bio && <span style={{ fontSize: '0.73em', marginTop: '0.5em' }}>"{foundCard.bio}"</span>}
             </div>
             {foundCard.hashtags && (
               <div
-                style={{ display: 'flex', alignItems: 'flex-start', gap: '0.5em', flexWrap: 'wrap', width: '200px' }}
+                style={{ display: 'flex', alignItems: 'flex-start', gap: '0.5em', flexWrap: 'wrap', width: '300px' }}
               >
                 {foundCard.hashtags.map((tag: string) => (
                   <span
@@ -82,12 +82,12 @@ export default async function handler(request: NextRequest) {
           >
             <img
               src={image_url?.signedUrl ?? 'https://pbs.twimg.com/media/DKi8pCTUEAA6X-o?format=jpg&name=small'}
-              width={150}
-              height={150}
+              width={120}
+              height={120}
               alt=""
               style={{
                 background: 'black',
-                width: '150px',
+                width: '120px',
                 borderRadius: '50%',
               }}
             />
@@ -108,7 +108,7 @@ export default async function handler(request: NextRequest) {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" width="18">
                     <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
                   </svg>
-                  <span style={{ width: '100px', wordBreak: 'break-all' }}>
+                  <span style={{ width: '130px', wordBreak: 'break-all' }}>
                     @{(foundCard.socialMedia as CardType['socialMedia'])?.github}
                   </span>
                 </div>
@@ -118,7 +118,7 @@ export default async function handler(request: NextRequest) {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="16">
                     <path d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z" />
                   </svg>
-                  <span style={{ width: '100px', wordBreak: 'break-all' }}>
+                  <span style={{ width: '170px', wordBreak: 'break-all' }}>
                     {(foundCard.socialMedia as CardType['socialMedia'])?.blog}
                   </span>
                 </div>
